b57d03edeebf3b1abff004573a38a8ae93a4af65
Fernando Giraldo Duque <ferchogir@gmail.com>
2017-11-14 23:09:27 -0500

    Update forms.js

--- plat_egresados/templates/forms.js
+++ plat_egresados/templates/forms.js
@@ -1,37 +1,40 @@
 function validarFormulario(){
- 
-        var txtNombre = document.getElementById('nombre').value;
-        var txtEdad = document.getElementById('edad').value;
-        var txtCorreo = document.getElementById('correo').value;
-        var txtFecha = document.getElementById('fecha').value;
-        var cmbSelector = document.getElementById('selector').selectedIndex;
-        var chkEstado = document.getElementById('checkBox');
+ 		
+ 		var txtUser = document.getElementById('username').value;
+        var txtPrimerNombre = document.getElementById('first_name').value;
+        var txtPrimerApellido = document.getElementById('last_name').value;
+        var txtSegundoApellido = document.getElementById('second_last_name').value;
+        var txtCorreo = document.getElementById('email').value;
+        var txtGender = document.getElementById('gender').selectedIndex;
  
  
         //Test campo obligatorio
-        if(txtNombre == null || txtNombre.length == 0 || /^\s+$/.test(txtNombre)){
+        if(txtPrimerNombre == null || txtPrimerNombre.length == 0 || /^[a-z A-Z]+$/.test(txtPrimerNombre)){
+            alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco');
+            return false;
+        }
+
+        if(txtPrimerApellido == null || txtPrimerApellido.length == 0 || /^[a-z A-Z]+$/.test(txtPrimerApellido)){
+            alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco');
+            return false;
+        }
+
+        if(txtSegundoApellido == null || txtSegundoApellido.length == 0 || /^[a-z A-Z]+$/.test(txtSegundoApellido)){
             alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco');
             return false;
         }
  
-        //Test edad
-        if(txtEdad == null || txtEdad.length == 0 || isNaN(txtEdad)){
-            alert('ERROR: Debe ingresar una edad');
-            return false;
-        }
- 
         //Test correo
-        if(!(/\S+@\S+\.\S+/.test(txtCorreo))){
-            alert('ERROR: Debe escribir un correo válido');
+        if(!(/\S+@utp.edu.co/.test(txtCorreo))){
+            alert('ERROR: Debe escribir un correo institucional (@utp.edu.co)');
             return false;
         }
- 
-        //Test fecha
-        if(!isNaN(txtFecha)){
-            alert('ERROR: Debe elegir una fecha');
-            return false;
-        }
- 
+
+        //Test comboBox
+		if(txtGender == null || txtGender == 0){
+			alert('ERROR: Debe seleccionar una opción');
+			return false;
+		}
  
         return true;
     }
